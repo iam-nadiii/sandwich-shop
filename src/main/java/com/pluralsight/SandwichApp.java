ff5d29093ad3574d0eb37b80075216f43e56eba1
iam-nadiii <naodd43@gmail.com>
2026-04-10 11:15:11 -0500

    initial code for the second exercise

--- src/main/java/com/pluralsight/SandwichApp.java
+++ src/main/java/com/pluralsight/SandwichApp.java
@@ -45,6 +45,21 @@ public class SandwichApp {
     }
 }
 
+//
+//Modify the sandwich-shop from the last exercise to allow customers to order a "loaded"
+//sandwich (double everything).
+//Step 1
+//Prompt the user for the size of the sandwich (1 or 2):
+//a. 1: Regular: base price $5.45
+//b. 2: Large: base price $8.95
+//Prompt the user if they would like the sandwich "loaded" (yes/no). If so there is an
+//additional cost for a loaded sandwich
+//a. Regular: $1.00
+//b. Large: $1.75
+//Prompt the user for their age:
+//        a. Student (17 years old or younger) – receive a 10% discount
+//b. Seniors (65 years old or older) – receive a 20% discount
+//Display the cost of the sandwich to the screen
 
 
 
